ca7d3f661d43f2a503f8d15fe324b33ac5124f92
GuilhermeRocha0404 <guilherme.trindade2010@gmail.com>
2022-05-31 01:45:32 -0300

    Atualização do Contador
    Eventos no HTML substituídos por eventos do javascript.

--- Contador/assets/scripts.js
+++ Contador/assets/scripts.js
@@ -1,6 +1,11 @@
 let count = 0;
 
 const CURRENT_NUMBER = document.getElementById('currentNumber');
+const SUBTRAI = document.getElementById("subtrair");
+const ADICIONA = document.getElementById("adicionar");
+
+ADICIONA.addEventListener("click", increment)
+SUBTRAI.addEventListener("click", decrement)
 
 function increment() {
 	count++;
@@ -10,8 +15,4 @@ function increment() {
 function decrement() {
 	count--;
 	CURRENT_NUMBER.innerHTML = count;
-}
-
-function test() {
-	kdowkdpo;
 }
