4f2943ced5e1bef615857e1f582bceed66ff232c
GuilhermeRocha0404 <guilherme.trindade2010@gmail.com>
2022-05-31 04:25:42 -0300

    Contador - Qualidade  de vida
    Os operadores dentro das condicionais "increment" e "decrement" foram alterados para obter uma leitura mais fácil do código.

--- Contador/assets/scripts.js
+++ Contador/assets/scripts.js
@@ -5,12 +5,14 @@ const SUBTRAIR = document.getElementById("subtrair");
 const ADICIONAR = document.getElementById("adicionar");
 
 ADICIONAR.addEventListener("click", increment);
-SUBTRAIR.addEventListener("click", decrement);
+// ADICIONAR.addEventListener("click", verificarNegativo)
+SUBTRAIR.addEventListener("click",decrement);
+// SUBTRAIR.addEventListener("click", verificarNegativo)
 
 function increment() {
 	count++;
 	CURRENT_NUMBER.innerHTML = count;
-	if(CURRENT_NUMBER.innerText > 4){
+	if(CURRENT_NUMBER.innerText === "5"){
 		ADICIONAR.disabled = true
 	}
 	else{
@@ -21,10 +23,22 @@ function increment() {
 function decrement() {
 	count--;
 	CURRENT_NUMBER.innerHTML = count;
-	if(CURRENT_NUMBER.innerText < -4){
+	if(CURRENT_NUMBER.innerText === "-5"){
 		SUBTRAIR.disabled = true
 	}
 	else{
 		ADICIONAR.disabled = false
 	}
 }
+
+// function verificarNegativo(){
+// 	if(CURRENT_NUMBER.innerText < "0"){
+// 		CURRENT_NUMBER.style.color = "red"
+// 	}
+// 	else if(CURRENT_NUMBER.innerText > "0"){
+// 		CURRENT_NUMBER.style.color = "green"
+// 	}
+// 	else{
+// 		CURRENT_NUMBER.style.color = "black"
+// 	}
+// }
